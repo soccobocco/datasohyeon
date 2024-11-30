2ce7fe375b3abcccf05d9de8d799fafafa590bac
소현 <dlthgus0416@naver.com>
2024-11-30 16:46:13 +0900

    Update two.app.py

--- two.app.py
+++ two.app.py
@@ -1,3 +1,4 @@
+
 import streamlit as st
 from pathlib import Path
 
@@ -7,21 +8,10 @@ html_file = Path('smoking_pmi_chart.html').read_text()
 # Streamlit에서 HTML 렌더링 (st.components.v1.html 사용)
 st.components.v1.html(html_file, height=600, width=800)
 
-# CSS로 공백 제거
-st.markdown("""
-    <style>
-        /* HTML 그래프가 포함된 div 태그에서 기본 마진 제거 */
-        .element-container {
-            margin: 0 !important;
-            padding: 0 !important;
-        }
-        /* 또는 st.components.v1.html로 렌더링된 HTML 그래프 하단 여백 제거 */
-        .stApp .block-container {
-            padding-bottom: 0 !important;  /* 하단 패딩 제거 */
-        }
-    </style>
-""", unsafe_allow_html=True)
+import streamlit as st
 
-# 추가적인 요소들로, 이미지 또는 다른 시각화 삽입
+# 올바른 Raw URL로 수정
 image_url = 'https://raw.githubusercontent.com/soccobocco/soccobocco.github.io/main/wordcloud2.png'
+
+# Streamlit에서 이미지 표시
 st.image(image_url)
