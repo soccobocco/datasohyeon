48def923cf4ca9ce5e2b35e73a40dcbf8608dc6b
소현 <dlthgus0416@naver.com>
2024-11-30 17:34:08 +0900

    Delete word.app.py

--- word.app.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-# 올바른 Raw URL로 수정
-image_url = 'https://raw.githubusercontent.com/soccobocco/soccobocco.github.io/main/wordcloud2.png'
-
-# Streamlit에서 이미지 표시
-st.image(image_url)
