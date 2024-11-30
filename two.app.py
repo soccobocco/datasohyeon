32ade85aef18fbb3778f4b72193785ca34983e5e
소현 <dlthgus0416@naver.com>
2024-11-30 16:38:17 +0900

    Update two.app.py

--- two.app.py
+++ two.app.py
@@ -1,1 +1,8 @@
+import streamlit as st
+from pathlib import Path
 
+# HTML 파일을 스트림릿에서 불러오기
+html_file = Path('smoking_pmi_chart.html').read_text()
+
+# Streamlit에서 HTML 렌더링 (st.components.v1.html 사용)
+st.components.v1.html(html_file, height=600, width=800)
