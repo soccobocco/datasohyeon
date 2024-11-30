95d7fb5f0050f52576825593dc5f302f5e047c41
소현 <dlthgus0416@naver.com>
2024-11-30 16:27:25 +0900

    Create word.app.py

---
+++ word.app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+# GitHub에서 복사한 이미지 URL (여기서 ?raw=true 추가)
+image_url = 'https://github.com/soccobocco/soccobocco.github.io/blob/main/wordcloud2.png?raw=true'
+
+# Streamlit에서 이미지 표시
+st.image(image_url)
+
