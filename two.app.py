a2725f3fa3efca24b173a0acdaee985f93420d8a
소현 <dlthgus0416@naver.com>
2024-11-30 16:42:16 +0900

    Update two.app.py

--- two.app.py
+++ two.app.py
@@ -7,7 +7,8 @@ html_file = Path('smoking_pmi_chart.html').read_text()
 # Streamlit에서 HTML 렌더링 (st.components.v1.html 사용)
 st.components.v1.html(html_file, height=600, width=800)
 
-import streamlit as st
+# HTML 요소 아래 마진 제거를 위한 스타일 추가
+st.markdown("<style>div[data-testid='stImage'] { margin-top: -10px; }</style>", unsafe_allow_html=True)
 
 # 올바른 Raw URL로 수정
 image_url = 'https://raw.githubusercontent.com/soccobocco/soccobocco.github.io/main/wordcloud2.png'
